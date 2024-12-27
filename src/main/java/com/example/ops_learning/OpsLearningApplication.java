442ea1a8f06984ff8eba8d8b68a3bb052c4b440e
Pedro Saavedra <pedrosaavedraemail@gmail.com>
2024-12-27 11:43:45 +0100

    Ops project for testing

---
+++ src/main/java/com/example/ops_learning/OpsLearningApplication.java
@@ -0,0 +1,23 @@
+package com.example.ops_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class OpsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsLearningApplication.class, args);
+	}
+
+	@RestController
+	public class Hello {
+		@GetMapping("/hello")
+		public String sayHello(){
+			return "Hello I m running :)";
+		}
+	}
+
+}
---
+++ src/test/java/com/example/ops_learning/OpsLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ops_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
